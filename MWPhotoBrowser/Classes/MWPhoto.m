2547c36c5ab1a44b67ae994e2fafbbaa0b783a77
Krzysztof Magiera <krzys@zonar.pl>
2013-07-11 13:12:17 +0200

    Support for images from assets library.

--- MWPhotoBrowser/Classes/MWPhoto.m
+++ MWPhotoBrowser/Classes/MWPhoto.m
@@ -8,6 +8,7 @@
 
 #import "MWPhoto.h"
 #import "MWPhotoBrowser.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 // Private
 @interface MWPhoto () {
@@ -15,6 +16,7 @@
     // Image Sources
     NSString *_photoPath;
     NSURL *_photoURL;
+    NSURL *_assetURL;
 
     // Image
     UIImage *_underlyingImage;
@@ -54,6 +56,10 @@ caption = _caption;
 	return [[[MWPhoto alloc] initWithURL:url] autorelease];
 }
 
++ (MWPhoto *)photoWithAssetURL:(NSURL *)url {
+	return [[[MWPhoto alloc] initWithAssetURL:url] autorelease];
+}
+
 #pragma mark NSObject
 
 - (id)initWithImage:(UIImage *)image {
@@ -73,6 +79,13 @@ caption = _caption;
 - (id)initWithURL:(NSURL *)url {
 	if ((self = [super init])) {
 		_photoURL = [url copy];
+	}
+	return self;
+}
+
+- (id)initWithAssetURL:(NSURL *)url {
+	if ((self = [super init])) {
+		_assetURL = [url copy];
 	}
 	return self;
 }
@@ -111,6 +124,9 @@ caption = _caption;
                                                           self.underlyingImage = image;
                                                           [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
                                                       }];
+	} else if (_assetURL) {
+            // Load async from asset library
+            [self performSelectorInBackground:@selector(loadImageFromAssetAsync) withObject:nil];
         } else {
             // Failed - no source
             self.underlyingImage = nil;
@@ -146,6 +162,32 @@ caption = _caption;
     } @finally {
         self.underlyingImage = [UIImage decodedImageWithImage: self.underlyingImage];
         [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+        [pool drain];
+    }
+}
+
+// Called in background
+// Load image in background from asset library
+- (void)loadImageFromAssetAsync {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    @try {
+        ALAssetsLibrary* assetslibrary = [[[ALAssetsLibrary alloc] init] autorelease];
+        [assetslibrary assetForURL:_assetURL
+                       resultBlock:^(ALAsset *asset){
+                           ALAssetRepresentation *rep = [asset defaultRepresentation];
+                           CGImageRef iref = [rep fullScreenImage];
+                           if (iref) {
+                               self.underlyingImage = [UIImage imageWithCGImage:iref];
+                           }
+                        [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+                       }
+                      failureBlock:^(NSError *error) {
+                          self.underlyingImage = nil;
+                          MWLog(@"Photo from file error: %@",error);
+                          [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+                      }];
+    } @catch (NSException *exception) {
+    } @finally {
         [pool drain];
     }
 }
